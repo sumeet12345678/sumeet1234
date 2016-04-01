bf9b2e9dbeb61c2d1a1d55f3b61f4dfabbe513ed
AmarChhetri <amar.work2016@gmail.com>
2016-04-01 23:38:09 +0530

    AmarCommit

---
+++ public/script/modal/aakComposer.js
@@ -0,0 +1,20 @@
+
+$(function(){
+	
+	$('#plus').on('click', function(){
+		var $composerWrapper = $('.composerWrapper');     
+		var $topMenubarHeight = $('.top-menubar').outerHeight();
+		var $footerBarHeight = $('#footerBarId').outerHeight();
+		var $viewPortHeight = $(window).height(); // height of browser viewport 
+		//alert($viewPortHeight - ($topMenubarHeight + $footerBarHeight));
+		//alert("window height == " + $viewPortHeight);
+		 
+		
+		$composerWrapper.toggleClass('visible');
+		$composerWrapper.css('bottom', $footerBarHeight); 
+		$composerWrapper.css('height', $viewPortHeight - ($topMenubarHeight+$footerBarHeight) - 5);
+		
+		//$composerWrapper.css('height',$footerBarHeight);  
+		//Get height for the viewport and substract header and footer height to get the required height for mobile devices
+	}); 
+});
--- public/script/modal/mPopUp_old.js
+++ public/script/modal/mPopUp_old.js
@@ -41,6 +41,9 @@ $(function(){
 	$('.modal-dialog').css('bottom', $('#footerBarId').height());
 	$('.modal-dialog').css('max-width', $('#profileDescription').width());
 	
+	
+	/* callBack functions implemented */
+	
 	function showSkillProfile(event){
 		console.log("Experience");
 		var xhr = new XMLHttpRequest();
@@ -50,20 +53,18 @@ $(function(){
 				console.log("Status  == 200");
 				$('.summary').html(xhr.responseText);
 				
-				$('.top-push-zone .pushContainer #leftBlock #offersIcon').attr('src','/media/images/icons/offers.png');  
-			//	$('#myModal').modal('hide');
+				updatePushZone();
 			}
 			else {
-				console.log("Status == " + xhr.status);
+				console.log("Status == " + xhr.status);  
 			}
 		}
 		
-		 xhr.open('GET','/data/skills_mihika.html', true);   
+		 xhr.open('GET','/data/skills_mihika.html', true);       
 		 xhr.send(null);
 		 // loads the content of the push zone
-		 $('.top-push-zone .pushContainer #leftBlock #offersIcon').attr('src','/media/images/icons/loading.gif');
-		 
-		 
+		 loadPushZone();
+		
 	}
 	
 	
@@ -75,6 +76,10 @@ $(function(){
 			if(xhr.status === 200){
 				console.log("Status  == 200");
 				$('.summary').html(xhr.responseText);
+				updatePushZone();
+			}
+			else {
+				console.log("Status == " + xhr.status);  
 			}
 			 
 		}
@@ -82,6 +87,7 @@ $(function(){
 		 //xhr.open('GET','http://localhost:3000/data/education_mihika.html', true);
 		xhr.open('GET','/data/education_mihika.html', true);
 		 xhr.send(null);
+		 loadPushZone();
 	}
 	
 	function showExperience(event){
@@ -92,6 +98,7 @@ $(function(){
 			if(xhr.status === 200){
 				console.log("Status  == 200");
 				$('.summary').html(xhr.responseText);
+				updatePushZone();
 			}
 			else {
 				console.log("Stauts == " + xhr.status);
@@ -100,14 +107,63 @@ $(function(){
 		
 		 xhr.open('GET','/data/experience_mihika.html', true);  
 		 xhr.send(null);
+		 loadPushZone();
 		 
 		// $('.modal-dialog').css('display','none');
 	}
 	
-	function showRecommendations(event){
-		 	}	
+	function showRecommendations(event) {   
+		console.log("Experience");
+		var xhr = new XMLHttpRequest();
+		
+		xhr.onload = function(){
+			if(xhr.status === 200){
+				console.log("Status  == 200");
+				//$('.summary').html(xhr.responseText);
+				
+				updatePushZone();
+			}
+			else {
+				console.log("Status == " + xhr.status);  
+			}
+		}
+		
+		 xhr.open('GET','/data/skills_mihika.html', true);       
+		 xhr.send(null);
+		 // loads the content of the push zone
+		 loadPushZone();
+		
+	}	
 	
-	function loadPushZoneContents(){
+
+	/*updates push zone*/
+	
+	function loadPushZone(){
+		
+		var content = '<img id="offersIcon" height="15" width="17" class="pull-left" style="margin-right: 10px; margin-top123: 0px" src="/media/images/icons/loading.gif" />' +
+				'<div style="display:inline-block; color: #77933C;"> Working on your action.. </div>';
+		
+		// $('.top-push-zone .pushContainer #leftBlock #offersIcon').attr('src','/media/images/icons/loading.gif');  
+		 //$('.top-push-zone .pushContainer #leftBlock').append("Working on your action..");
+			//
+		   $('.top-push-zone .pushContainer').addClass("pushZoneActionMessage");
+		   $('.top-push-zone .pushContainer #leftBlock').html(content);
 		
 	}
+	
+	function updatePushZone(){    
+		/*delay inserted for test purpose only */  
+		setTimeout(function(){
+			var pushContent = '<img id="offersIcon" height="15" width="17" class="pull-left" style="margin-right: 10px; margin-top123: 0px" src="/media/images/icons/offers.png" />' + 
+				'<div style="display:inline-block;"> 3578/- of referrals, 29317/- of discounts.. <span class="blue-on-top-of-earth-color arrow-link">&gt;&gt;</span></div>';
+			
+				//$('.top-push-zone .pushContainer #leftBlock #offersIcon').attr('src','/media/images/icons/offers.png');
+				//$('.top-push-zone .pushContainer #leftBlock').append($pushContent);
+			$('.top-push-zone .pushContainer #leftBlock').html(pushContent);
+			 $('.top-push-zone .pushContainer').removeClass("pushZoneActionMessage");
+				
+			},1000);
+		 
+	}
+	
 });
