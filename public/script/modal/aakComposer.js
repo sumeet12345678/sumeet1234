bcec7592e6de6408a01a445fd075ff119360a1fb
AmarChhetri <amar.work2016@gmail.com>
2016-04-03 00:50:38 +0530

    AmarCommit

--- public/script/modal/aakComposer.js
+++ public/script/modal/aakComposer.js
@@ -13,7 +13,7 @@ $(function(){
 		$composerWrapper.toggleClass('visible');
 		$composerWrapper.css('bottom', $footerBarHeight); 
 		$composerWrapper.css('height', $viewPortHeight - ($topMenubarHeight+$footerBarHeight) - 309);
-		
+				
 		//$composerWrapper.css('height',$footerBarHeight);  
 		//Get height for the viewport and substract header and footer height to get the required height for mobile devices
 	}); 
