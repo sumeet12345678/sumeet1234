cb95e9557a265482d7fbf4f5d10131d25ad1e0a7
AmarChhetri <amar.work2016@gmail.com>
2016-05-02 03:14:12 +0530

    AmarCommit

--- public/script/orgEstate/orgEstate.js
+++ public/script/orgEstate/orgEstate.js
@@ -23,11 +23,25 @@ $(function(){
 		  $('#ORG_EST_MSTR_MENU_EXIT_MENU').toggle();
 	}); 
 	
-	var SCREEN_WIDTH = screen.width;
+	//var SCREEN_WIDTH = screen.width;
 	
 	//$('#ORG-ESTATE_SEARCH_BOX').css('left', ((SCREEN_WIDTH - SCREEN_WIDTH * .30) - 300)/2);
 	$('#ORG-ESTATE_SEARCH_BOX').css('left', "50%");
 	$('#ORG-ESTATE_SEARCH_BOX').css('left', "-=150");
+	
+	$('#ORG_ESTATE_SLIDING_MENU_ICON').on('click', function(){
+		//alert('dfdfdfdfdfdf');
+		//$('#ORG_ESTATE_SLIDING_MENU_BAR').toggle();  
+		var element = document.getElementById('ORG_ESTATE_SLIDING_MENU_BAR'); 
+		//alert("dfdfd == " + element.style.display=="inline-block");	
+		if(element.style.display == "none"){
+			element.style.display = "inline-block";
+		}
+		else {
+			element.style.display = none;
+		}
+		
+	});
 	
 	function displayOrgEstateMstrMenu(){
 		var orgEstMstrMenuSingleTon = $('#ORG_EST_MSTR_MENU_SINGLETON');
