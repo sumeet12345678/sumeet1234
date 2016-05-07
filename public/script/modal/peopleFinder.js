a68f53b64d6eaded3711f7388abc1b3a7779f187
AmarChhetri <amar.work2016@gmail.com>
2016-05-08 00:21:57 +0530

    AmarCommit

--- public/script/modal/peopleFinder.js
+++ public/script/modal/peopleFinder.js
@@ -4,14 +4,20 @@ $(function() {
 	var peopleFinderIcon =  $('#PEOPLE_FINDER_ICON');
 	var peopleFinder =  $('#PEOPLE_FINDER'); 	 //popUp	PEOPLE_FINDER_MIN_ICON
 	var peopleFinderMinIcon=  $('#PEOPLE_FINDER_MIN_ICON');
+	var peopleFinderOptionsCloseBtn = $('.peopleFinderOptionsCloseBtn');
+	
 	//peopleFinderSearchButton.css('bottom',FOOTER_BAR_HEIGHT );
 	peopleFinderIcon.on('click', findPeople);
 	
 	
-	peopleFinderMinIcon.on('click', function(){
+	peopleFinderMinIcon.on('click', function() {
 		peopleFinder.toggle(); //remove duplicacy  
 	});
 	
+	peopleFinderOptionsCloseBtn. on('click', function() {
+			//$(this).parent().css({"color": "red", "border": "2px solid red"});
+			$(this).parent().css('display', 'none');
+	});
 	
 	function findPeople() {  
 		 peopleFinder.toggle();
