bfd8855d347df5d30634296b8e643970ee6f126c
AmarChhetri <amar.work2016@gmail.com>
2016-05-22 02:54:13 +0530

    Commit1

--- routes/route.js
+++ routes/route.js
@@ -19,6 +19,11 @@ router.get('/profile/view/mahika', function(req, res, next) {
 		//res.render('profile/profile.ejs');
 	});
 
+router.get('/profile/view/lawyer', function(req, res, next) {
+	  res.render('profiles/common.ejs'); 
+		//res.render('profile/profile.ejs');
+	});
+
 router.get('/aakBox', function(req, res, next) {
 	  var categoryId = req.param('categoryId');
 	     // console.log("Category from req ===  " + categoryId);
@@ -34,22 +39,52 @@ router.get('/estate', function(req, res, next) {
 
 router.get('/view-aak', function(req, res, next) {
 	  res.render('viewAak/viewAak.ejs');	    
-	});   
+	});  
+
+
 /*
 router.get('/profile/view/mahika/experience', function(req, res, next) {
 	  res.render('profiles/summary/experience_mihika.ejs'); 
 	});
 */
 
+
+
+var profileJsonData = [{
+	"profile_header" : [  { "pic" : "/media/images/profile_images/Mihika_JustDial.jpg",	                        
+	                        "pic-size" : "passport", //s, passport or Visa size
+	                        "profile_pic_position" : "right" ,		//left, center or right
+	                        "background-color" : "#333",
+	                        
+	    					},
+	    					
+	    					{
+	    						"quote" : [{"text" : "Worrying is misuse of imagination...",
+	    						            "text-box-type" : "rectangular"  //square or rectangle 
+	    						             
+	    								 }]
+	    					}
+                       ],
+    
+    "profile_title" : "Software Developer.... :) ",
+    
+    "profile_summary" : "SummarySummary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary" ,
+    
+    "profile_sections" : [{
+ 	   	
+ 	   
+    					}]
+}];
+
 /* GET people_with_pic.ejs file. */
 router.get('/uniprofile', function(req, res, next) {
-  res.render('profiles/common.ejs');   
+  res.render('profiles/common.ejs', {user:"John Smith"});   
 });
 
 
 //Trial below
 router.get('/trial', function(req, res, next) {
-	  res.render('profiles/trial.ejs');   
+	  res.render('profiles/trial.ejs');
 	});
 
 
