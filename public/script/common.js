9a6719e6da9bcb948458e6c8c31c0519a76c4a55
AmarChhetri <amar.work2016@gmail.com>
2016-06-24 02:11:08 +0530

    Commit1

--- public/script/common.js
+++ public/script/common.js
@@ -8,6 +8,7 @@
 	//alert(" -----"  + CENTER_REGION_HEIGHT);  
 	//alert(" ----ttt-"  + TOP_MENU_BAR_HEIGHT);
 	var _g_blurScreen;
+	var g_pageHeaderBarJSONData; // carries page title, features icons menu JSON 
    
 
  
@@ -103,52 +104,57 @@ function unBlurScreen() {
 	_g_blurScreen.hide();  
 }
 
-/* For adding header bar to all pages */
-function initHeaderBar(title, headerBar, headerBarJsonData) {   
-	//alert("hello");
-	var closeBtn = $('<div style = "display: inline-block; position:absolute; font-weight: bold; font-size: 14px; top:0; left:0;">X</div>');
-	//headerBar.append(closeBtn);
-	headerBar.append('<b>' + title + '</b>');    
-	
+/* 
+ * 
+ * For init-ing header bar in pages 
+ * 
+ */
+function initHeaderBar() {
+	var headerBar = $('#CONTENT_HEADER_BAR');
+	headerBar.append('<b>' + g_pageHeaderBarJSONData.title + '</b>'); 
+	if(g_pageHeaderBarJSONData.features.length > 0) {
+		document.getElementById('CONTENT_HEADER_BAR_RT_ICON').style.display = 'block';
+	}	
+	headerBar.show();
+}
+
+/* 
+ * 
+ * common icons menu creator and toggler
+ * 
+ */
+function iconsMenuLauncherClickHandler(launchersIdentifer, iconsDataJson) {
+	if(!iconsDataJson) {
+		iconsDataJson = g_pageHeaderBarJSONData.features;
+	}
+	var iconsMenuID = launchersIdentifer  + '_MENU';
+	var iconsMenuObj = document.getElementById(iconsMenuID);
+	if(iconsMenuObj == null) {
+		$('#CONTENT_HEADER_BAR').append(createIconMenu(iconsMenuID, iconsDataJson));
+	} else {		
+		$(iconsMenuObj).toggle();
+	}
+}
+
+/*function placeIcon() {
 	var menuDropDownDiv = $('<div class = "menu-drop-down-icon" style = "display:inline-block; height: 30px; top: 0; position: absolute; right: 0;"></div>'); 
 	var menuDropDownImgIcon = $('<img src = "/media/images/icons/plainblack/3lines-thin-menu-blk.png" width = 30px; height = 30px;" />');
 	menuDropDownDiv.append(menuDropDownImgIcon);
 
+}*/
+
+
+function createIconMenu(iconsMenuID, iconsInfoJson) {	
 	/* append images(menu items) to menu items container */
-	var menuItemsContainer = $('<div class = "cmn_mnu_itm_cntnr" style = "background-color : #FFF ; display: none; position: absolute; right: 0;border: 2px solid #dcdcdc;"></div>');
-	 
-	
-	var menuItemsHtmlString  = "";
-	
-	for(var i = 0; i < headerBarJsonData.length; i++) {
-		menuItemsHtmlString = '<div class = "img-with-caption-common" data-url-value = ' +  headerBarJsonData[i].url  + '>';
-		//console.log("url value == " + headerBarJsonData[i].url);
-		menuItemsHtmlString = menuItemsHtmlString + '<img style = "cursor: pointer;"  width="28" height="28" src="' + headerBarJsonData[i].img +  '" />';   
-		menuItemsHtmlString = menuItemsHtmlString +  '<span class = "img-caption"><b>Opportunities</b></span>' + '</div>';
-		
+	var menuItemsContainer = $('<div id = "' + iconsMenuID  + '" style = "background-color : #FFF ; display: block; position: absolute; right: 0;border: 2px solid #dcdcdc;"></div>');
+	var menuItemsHtmlString  = "";	
+	for(var i = 0; i < iconsInfoJson.length; i++) {
+		menuItemsHtmlString = '<div class = "img-with-caption-common" onClick = "javascript: location.href = \'' + iconsInfoJson[i].url + '\';">';		
+		menuItemsHtmlString = menuItemsHtmlString + '<img style = "cursor: pointer;"  width="28" height="28" src="' + iconsInfoJson[i].img +  '" />';   
+		menuItemsHtmlString = menuItemsHtmlString +  '<span class = "img-caption"><b>' + iconsInfoJson[i].txt +'</b></span>' + '</div>';		
 		menuItemsContainer.append($(menuItemsHtmlString));  
 	}
-	   	
-  
-	
-/*	
-	<div class = "img-with-caption-common">
-		<img   width="28" height="28" src="/media/images/icons/plainblack/opportunities-blk.png" />
-	    <span class = "img-caption" style = ""><b>Opportunities</b></span>  
-    </div>  
-*/	
-	
-	
-	
-	
-	//menuDropDownDiv.append(menuItemsContainer);
-	headerBar.append(menuItemsContainer);
-//	var menuDropDownDiv = "<div>" +
-//						'<div class = "img-with-caption-common"><img   width="28" height="28" src="/media/images/icons/plainblack/talks-trending-blk.png" /><span class = "img-caption"><b>Trending</b></span></div> ' + "</div>";  
-	headerBar.append(menuDropDownDiv);
-	headerBar.show();
-	
-	
+	return menuItemsContainer;
 }
 
 
