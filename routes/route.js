7c097614019f24cdcc7b8c56d5ff67b5a0e7e17f
deka4tech <deka4tech@gmail.com>
2016-08-14 16:10:33 +0530

    dfdfcv

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 //var g_hostUrl = "http://ekpeople.herokuapp.com" ;
-var g_hostUrl = "http://ekpeople.herokuapp.com" ;
-//var g_hostUrl = "http://localhost:4000" ;
+//var g_hostUrl = "http://ekpeople.herokuapp.com" ;
+var g_hostUrl = "http://localhost:4000" ;
 var g_profileHostUrl = "http://ekprofile.herokuapp.com" ;
  
 
